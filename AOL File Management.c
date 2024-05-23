ffeede4a08b45984fba0e1530dfb52db4427f20e
Trid3nz <88418663+Trid3nz@users.noreply.github.com>
2024-05-23 13:11:25 +0700

    buat templete

---
+++ AOL File Management.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 30
+//in each data, there is quantity, name, category
+struct data{
+    int quantity;
+    char name[MAX];
+    char category[MAX];
+    struct data *next;    
+};
+
+struct node *head, *node, *temp;
+//Menu 
+void menu(){
+    printf("================================================");
+    printf("================Warehouse Inventory=============");
+    printf("1. Input Data");
+    printf("2. View Data");
+    printf("3. Delete Data");
+    printf("================================================");
+
+}
+
+int main(){
+    int m;
+    do
+    {
+        menu();
+        do{
+            printf(">>");scanf("%d",&m);
+        }while(m > 9);
+        switch(m){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break; 
+        }
+    }while(m != 0);
+}
